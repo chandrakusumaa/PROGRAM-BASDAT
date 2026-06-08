8020ce7ed5fc759f5a417c9837c5c45f79e5ffc5
Your Name <amalabdillahwildan@gmail.com>
2026-06-08 16:05:01 +0700

    view

--- ProgramConfig.php
+++ ProgramConfig.php
@@ -43,3 +43,88 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
  </form>
 </body>
 </html>
+
+
+<?php
+// Konfigurasi koneksi database
+$conn = pg_connect(
+    "host=localhost
+    port=5432
+    dbname=db_perpustakaan
+    user=postgres
+    password=w1lmaL06!?"
+);
+
+// Cek koneksi
+if (!$conn) {
+    die("Koneksi PostgreSQL gagal");
+}
+// Ambil data dari tabel
+$sql = "SELECT * FROM tbl_buku ORDER BY id_buku DESC";
+$result = pg_query($conn, $sql);
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Data Buku Perpustakaan</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        table, td, th {
+            border: 1px solid #000;
+            padding: 8px;
+        }
+        th {
+            background-color: #eee;
+        }
+    </style>
+</head>
+<body>
+    <h2>Daftar Buku Perpustakaan</h2>
+    <a href="add.php">+ Tambah Buku Baru</a>
+    <br><br>
+
+    <table>
+    <tr>
+        <th>No</th>
+        <th>id_buku</th>
+        <th>judul</th>
+        <th>Penulis</th>
+        <th>Tahun Terbit</th>
+        <th>Aksi</th>
+    </tr>
+ <?php
+if (pg_num_rows($result) > 0) {
+
+    $no = 1;
+
+    while ($row = pg_fetch_assoc($result)) {
+
+        echo "<tr>
+            <td>".$no++."</td>
+            <td>".$row['id_buku']."</td>
+            <td>".$row['judul']."</td>
+            <td>".$row['penulis']."</td>
+            <td>".$row['tahun_terbit']."</td>
+            <td>
+                <a href='edit.php?id=".$row['id_buku']."'>Edit</a> |
+                <a href='delete.php?id=".$row['id_buku']."'
+                onclick=\"return confirm('Yakin ingin menghapus?')\">Hapus</a>
+            </td>
+        </tr>";
+    }
+
+} else {
+
+    echo "<tr>
+            <td colspan='6'>Belum ada data buku.</td>
+          </tr>";
+}
+?>
+    </table>
+</body>
+</html>
